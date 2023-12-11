72107a3fd4d54dad63116bd8880017141e7f2942
nuboctane <renzoheesen@gmail.com>
2023-12-11 10:03:36 +0100

    formatting

--- GUI.py
+++ GUI.py
@@ -3,13 +3,26 @@ import ttk
 from PIL import ImageTk, Image
 from JSON import json_parser as JSON
 import threading
+
 class gui_class():
     def open_gui():
         root = Tk()
         root.configure(background='#171d25')
-
         icon_frame = Frame(root, bg="#ffc917")
         icon_frame.pack()
+
+        def update(ind):
+            frame = loading_frames[ind]
+            label.configure(image=frame)
+            ind += 1
+            if ind == frame_count:
+                ind = 0
+            root.after(10, update, ind)
+        
+        frame_count = 63
+        loading_frames = [PhotoImage(file='assets/qwe_download.gif', format='gif -index %i' % i) for i in range(frame_count)]
+        label = Label(root)
+        label.pack()
 
         # GUI data updaten terwijl we in root.mainloop zitten
         print("updating...")
@@ -17,6 +30,7 @@ class gui_class():
         main_update_thread.start()
         # main_update_thread.join()
 
+        update(0)
         root.title("project steam")
         root.geometry('700x450')
         root.mainloop()
